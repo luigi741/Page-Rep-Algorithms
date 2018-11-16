e761b40e93a6aa6fc84fd6b85dec26e58b58602d
Luis Castro <luigi_789@hotmail.com>
2018-11-16 16:26:22 -0500

    First couple pages replacing properly

--- pageRep.cpp
+++ pageRep.cpp
@@ -27,7 +27,7 @@ int main()
     for (auto x: pageRef) {
         cout << x << "|";
     }
-    cout << endl;
+    cout << endl << endl;
     FIFO(pageRef, pageRef.size());
     return 0;
 }
@@ -42,24 +42,84 @@ void FIFO(deque<int> pageQueue, int arrLength)
         }
     }
 
-    cout << "\nFront of Queue: " << pageQueue.front() << endl;
-
     int frameArray[3] = {0, 0, 0};
     int counter[3] = {0, 0, 0};
+    int popCount = 0;
 
     // Adding the first pages (which are page faults)
     for (int i = 0; i < 3; i++) {
         frameArray[i] = pageQueue.front();
         pageQueue.pop_front();
+        popCount++;
         for (int j = 0; j < 3; j++) {
             table[i][j] = frameArray[j];
         }
     }
 
+    // Adding subsequent pages
+    for (int i = 0; i < popCount; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (table[j][i] == frameArray[i]) {
+                counter[i]++;
+            }
+        }
+    }
+
+    // Print table
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 3; j++) {
             cout << table[i][j] << " ";
         }
         cout << endl;
+    }
+
+    // Print counter array
+    cout << "\nCounter array: ";
+    for (int i = 0; i < 3; i++) {
+        cout << counter[i] << " ";
+    }
+    cout << endl;
+
+    // Find largest element/index in counter[] which will tell us which page to
+    // replace
+    int maxValue = counter[0];
+    int maxIndex = 0;
+    for (int i = 0; i < 3; i++) {
+        if (maxValue < counter[i]) {
+            maxValue = counter[i];
+            maxIndex = i;
+        }
+    }
+
+    cout << "Largest element: " << maxValue << endl;
+    cout << "Largest index: " << maxIndex << endl;
+
+    // Calculate which page to replace next based on info from above
+    frameArray[maxIndex] = pageQueue.front();
+    pageQueue.pop_front();
+    popCount++;
+
+    cout << "Frame array: ";
+    for (int i = 0; i < 3; i++) {
+        cout << frameArray[i] << " ";
+    }
+
+    cout << "\nRow index: " << popCount << endl;
+
+    for (int i = 0; i < 3; i++) {
+        table[popCount][i] = frameArray[i];
+    }
+
+    // Dealing with PAGE HITS
+    for (int i = 0; i < 3; i++) {
+        if (pageQueue.front() == frameArray[i]) {
+            table[popCount][i] == table[popCount-1][i];
+        }
+    }
+    pageQueue.pop_front();
+    popCount++;
+    cout << "Frame array: ";
+    for (int i = 0; i < 3; i++) {
+        cout << frameArray[i] << " ";
     }
 }
