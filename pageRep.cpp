18b07ba6bb20a0007be5eb8e2527258248e0f285
Luis Castro <luigi_789@hotmail.com>
2018-11-15 23:32:10 -0500

    Implemented C++ 'deque' library

--- pageRep.cpp
+++ pageRep.cpp
@@ -4,35 +4,35 @@
 // By: Luis Castro
 //==============================================================================
 #include <iostream>
+#include <cmath>
+#include <deque>
 using namespace std;
 
-void FIFO(int arr[], int arrLength);
+void FIFO(deque<int> pageQueue, int arrLength);
 
 int main()
 {
-    int pageRef[10];
+    int tempVar = 0;
+    deque<int> pageRef;
 
     // First, let's get integers for the page references
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 5; i++) {
         cout << "Page " << i + 1 << ": ";
-        cin  >> pageRef[i];
+        cin  >> tempVar;
+        pageRef.push_back(tempVar);
     }
 
     cout << "\nNumbers have been entered, here's the string: |";
 
-    int length = (sizeof(pageRef)/sizeof(*pageRef));
-
-    for (int i = 0; i < length; i++) {
-        cout << pageRef[i] << "|";
+    for (auto x: pageRef) {
+        cout << x << "|";
     }
-    cout << "\n\n";
-
-    FIFO(pageRef, length);
-
+    cout << endl;
+    FIFO(pageRef, pageRef.size());
     return 0;
 }
 
-void FIFO(int arr[], int arrLength)
+void FIFO(deque<int> pageQueue, int arrLength)
 {
     // Initialize table, set values to 0
     int table[10][3];
@@ -42,52 +42,22 @@ void FIFO(int arr[], int arrLength)
         }
     }
 
+    cout << "\nFront of Queue: " << pageQueue.front() << endl;
+
     int frameArray[3] = {0, 0, 0};
     int counter[3] = {0, 0, 0};
-    // int marker = 0;
 
-    for (int i = 0; i < 10; i++)
-    {
-        // Replacing the pages in the frames and then copying into the table
-        for (int j = 0; j < 3; j++)
-        {
-            if (i < 3) {
-                frameArray[i] = arr[i];
-            }
-            else {
-                // Check for FIFO rule
-                for (int x = 0; x < 3; x++) {
-                    for (int y = i; y >= 0; y--) {
-                        if (table[y][x] == frameArray[x] && i == 5) {
-                            counter[x]++;
-                            cout << "table[" << y << "][" << x << "]";
-                            cout << "\ti: " << i << "\tj: " << j;
-                            cout << "\tx: " << x << "\tx: " << x << endl;
-                        }
-                    }
-                }
-            }
+    // Adding the first pages (which are page faults)
+    for (int i = 0; i < 3; i++) {
+        frameArray[i] = pageQueue.front();
+        pageQueue.pop_front();
+        for (int j = 0; j < 3; j++) {
             table[i][j] = frameArray[j];
-
-            // if (marker == j) {
-            //     frameArray[j] = arr[i];
-            // }
-            // table[i][j] = frameArray[j];
         }
-        if (i == 5) {
-            cout << "\nCounter Array: ";
-            cout << counter[0] << " " << counter[1] << " " << counter[2] << "\n\n";
-        }
-        // marker++;
-        // if (marker >= 3) {
-        //     marker %= 3;
-        // }
     }
 
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 3; j++) {
             cout << table[i][j] << " ";
         }
         cout << endl;
